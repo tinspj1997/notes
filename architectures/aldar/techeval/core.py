cbd4b0e92671121716e4e9ddb0d75ce137d8b5c8
tins.joseph <tins.joseph@gapblue.com>
2026-06-10 22:52:24 +0530

    diagram

--- architectures/aldar/techeval/core.py
+++ architectures/aldar/techeval/core.py
@@ -1,7 +1,33 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import ECS
+from diagrams.aws.compute import ComputeOptimizer
+from diagrams.onprem.queue import Rabbitmq, RabbitMQ
+from diagrams.onprem.inmemory import Redis
 from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams.aws.analytics import CloudsearchSearchDocuments
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+
+with Diagram("Tech Eval Architecture", show=False, direction="TB",outformat="jpg"):
+    producer = ComputeOptimizer("Producer")
+    RMQQueue = RabbitMQ("Rabbit MQ")
+
+    with Cluster("Evaluation Worker"):
+        eval_workers = [
+             ECS("Embedding"),
+                CloudsearchSearchDocuments("Extractor"),
+                ECS("Downloader")
+               ]
+        
+    with Cluster("RFP Worker"):
+        rfp_workers = [
+             ECS("Embedding"),
+                ECS("Extractor"),
+                ECS("Downloader")
+               ]
+        
+        
+
+
+    producer >> RMQQueue >> eval_workers
+    producer >> RMQQueue >> rfp_workers
+    RMQQueue >> Redis("Queue State")
