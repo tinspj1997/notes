c802f0f5b6be82f74afe06aa884a81fa3cfaf3c2
tins.joseph <tins.joseph@gapblue.com>
2026-06-10 23:00:42 +0530

    queue added

--- architectures/aldar/techeval/core.py
+++ architectures/aldar/techeval/core.py
@@ -5,11 +5,16 @@ from diagrams.onprem.queue import Rabbitmq, RabbitMQ
 from diagrams.onprem.inmemory import Redis
 from diagrams.aws.database import RDS
 from diagrams.aws.analytics import CloudsearchSearchDocuments
+from diagrams.aws.network import ELB
+from diagrams.aws.integration import SQS
 
 
 with Diagram("Tech Eval Architecture", show=False, direction="TB",outformat="jpg"):
     producer = ComputeOptimizer("Producer")
     RMQQueue = RabbitMQ("Rabbit MQ")
+    RoutingExchange = ELB("Routing Exchange")
+    RFPQueue = SQS("RFP Queue")
+    EvalQueue = SQS("Eval Queue")
 
     with Cluster("Evaluation Worker"):
         eval_workers = [
@@ -28,6 +33,6 @@ with Diagram("Tech Eval Architecture", show=False, direction="TB",outformat="jpg
         
 
 
-    producer >> RMQQueue >> eval_workers
-    producer >> RMQQueue >> rfp_workers
+    producer >> RoutingExchange >>RMQQueue >> EvalQueue >> eval_workers
+    producer >> RoutingExchange >> RMQQueue >> RFPQueue >> rfp_workers
     RMQQueue >> Redis("Queue State")
